e0d963b00650f8ebc77689f583e5278776788d20
Владимир <davidvovchik@gmail.com>
2023-12-27 10:40:09 +0200

    rightside

--- src/containers/Main/Main.js
+++ src/containers/Main/Main.js
@@ -2,6 +2,7 @@ import './Main.scss';
 import MainHeader from '../../components/MainHeader/MainHeader';
 import MainLeftSide from '../../components/MainLeftSide/MainLeftSide';
 import MainSlider from '../../components/MainSlider/MainSlider';
+import MainRightSide from '../../components/MainRightSide/MainRightSide';
 
 function Main() {
   return (
@@ -9,6 +10,7 @@ function Main() {
       <MainHeader />
       <MainLeftSide />
       <MainSlider />
+      <MainRightSide />
     </div>
   );
 }
