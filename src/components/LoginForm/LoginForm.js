2d445c671064b295cc19315b122de0f2641b2262
Владимир <davidvovchik@gmail.com>
2023-12-30 15:10:19 +0200

    update

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,9 +1,32 @@
 import './LoginForm.scss';
+import React from 'react';
+import { useFormik } from 'formik';
 
 const LoginForm = () => {
+
+    const formik = useFormik({
+        initialValues: {
+            email: '',
+        },
+        onSubmit: values => {
+            alert(JSON.stringify(values, null, 2));
+        },
+    });
+
     return (
         <div className='login__form__wrap'>
-            LoginForm
+            <form onSubmit={formik.handleSubmit} className='login__formik'>
+                <label htmlFor="email">Email Address</label>
+                <input
+                    id="email"
+                    name="email"
+                    type="email"
+                    onChange={formik.handleChange}
+                    value={formik.values.email}
+                />
+
+                <button type="submit">Submit</button>
+            </form>
         </div>
     )
 }
