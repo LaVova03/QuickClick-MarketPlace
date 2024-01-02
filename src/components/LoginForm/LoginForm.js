d296e234efa416b376f53e2586798a1508a47c9e
Владимир <davidvovchik@gmail.com>
2024-01-02 16:31:29 +0200

    login

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,33 +1,67 @@
 import './LoginForm.scss';
 import React from 'react';
 import { useFormik } from 'formik';
+import { useNavigate } from "react-router-dom";
 
 const LoginForm = () => {
+
+    const navigate = useNavigate();
 
     const formik = useFormik({
         initialValues: {
             email: '',
+            password: '',
         },
         onSubmit: values => {
             alert(JSON.stringify(values, null, 2));
         },
     });
 
+    const handleNavigate = () => navigate("/");
+
+    const HandleClick = (e) => {
+        e.preventDefault();
+    }
+
     return (
         <div className='login__form__wrap'>
             <form onSubmit={formik.handleSubmit} className='login__formik'>
-                <label htmlFor="email">Email Address</label>
+                <button className='login__btn__back' onClick={handleNavigate}></button>
+                <button onClick={HandleClick} className='login__btn__google'><div className='login__google' /> Продовжити з Gmail</button>
+                <button onClick={HandleClick} className='login__btn__inst'><div className='login__inst' /> Продовжити з Instagram</button>
+                <button onClick={HandleClick} className='login__btn__facebook'><div className='login__facebook' /> Продовжити з Facebook</button>
+                <span>або</span><br />
+                <div className='login__btn__wrap'>
+                    <button onClick={HandleClick}>Зареєструватися</button>
+                    <button onClick={HandleClick}>Увійти</button>
+                </div>
                 <input
                     id="email"
                     name="email"
                     type="email"
                     onChange={formik.handleChange}
                     value={formik.values.email}
-                />
-
-                <button type="submit">Submit</button>
+                    placeholder='Електронна пошта чи телефон'
+                /><br />
+                <input
+                    id="password"
+                    name="password"
+                    type="password"
+                    onChange={formik.handleChange}
+                    value={formik.values.password}
+                    placeholder='Пароль*'
+                /><br />
+                <button
+                    className='login__forgot__pasword'
+                    onClick={HandleClick}>Забули пароль?
+                </button>
+                <button
+                    type="submit"
+                    className='login__summit'>
+                    Увійти
+                </button>
             </form>
-        </div>
+        </div >
     )
 }
 
