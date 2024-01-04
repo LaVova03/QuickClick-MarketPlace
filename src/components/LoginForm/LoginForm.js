504b0b5956b0547e33d36557cfa31c92dc27d178
Владимир <davidvovchik@gmail.com>
2024-01-04 13:33:24 +0200

    update

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,24 +1,13 @@
 import './LoginForm.scss';
 import React, { useState } from 'react';
-import { useFormik } from 'formik';
-import { useNavigate } from "react-router-dom";
+import { Formik, Field, Form, ErrorMessage } from 'formik';
+import { useNavigate } from 'react-router-dom';
+import { Form as BootstrapForm } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const LoginForm = () => {
-
     const [registration, setRegistration] = useState(false);
-
     const navigate = useNavigate();
-
-    const formik = useFormik({
-        initialValues: {
-            email: '',
-            password: '',
-            checkbox: false,
-        },
-        onSubmit: values => {
-            alert(JSON.stringify(values, null, 2));
-        },
-    });
 
     const handleNavigate = () => navigate("/");
 
@@ -38,56 +27,73 @@ const LoginForm = () => {
 
     return (
         <div className='login__form__wrap'>
-            <form onSubmit={formik.handleSubmit} className='login__formik'>
-                <button className='login__btn__back' onClick={handleNavigate}></button>
-                <button onClick={HandleClick} className='login__btn__google'><div className='login__google' /> Продовжити з Gmail</button>
-                <button onClick={HandleClick} className='login__btn__inst'><div className='login__inst' /> Продовжити з Instagram</button>
-                <button onClick={HandleClick} className='login__btn__facebook'><div className='login__facebook' /> Продовжити з Facebook</button>
-                <span>або</span><br />
-                <div className='login__btn__wrap'>
-                    <button onClick={showRegistration}>Зареєструватися</button>
-                    <button onClick={showEnterence}>Увійти</button>
-                </div>
-                <input
-                    id="email"
-                    name="email"
-                    type="email"
-                    onChange={formik.handleChange}
-                    value={formik.values.email}
-                    placeholder='Електронна пошта чи телефон'
-                    autoComplete="email"
-                /><br />
-                <input
-                    id="password"
-                    name="password"
-                    type="password"
-                    onChange={formik.handleChange}
-                    value={formik.values.password}
-                    placeholder='Пароль*'
-                    autoComplete="password"
-                /><br />
-                {!registration ?
-                    <button
-                        className='login__forgot__pasword'
-                        onClick={HandleClick}>Забули пароль?
-                    </button> :
-                    <div className='login__checkbox'>
-                        <input
-                            id="checkbox"
-                            name="checkbox"
-                            type="checkbox"
-                            checked={formik.values.checkbox}
-                            onChange={formik.handleChange}
-                        />
-                        Створюючи профіль на QuickQlick, ви погоджуєтеся з умовами використання
-                    </div>}
-                <button
-                    type="submit"
-                    className='login__summit'>
-                    {registration ? 'Створити' : 'Увійти'}
-                </button>
-            </form>
-        </div >
+            <Formik
+                initialValues={{
+                    email: '',
+                    password: '',
+                    checkbox: false,
+                }}
+                onSubmit={values => {
+                    alert(JSON.stringify(values, null, 2));
+                }}
+            >
+                {({ handleSubmit, handleChange, values }) => (
+                    <Form onSubmit={handleSubmit} className='login__formik'>
+                        <button className='login__btn__back' onClick={handleNavigate}></button>
+                        <button onClick={HandleClick} className='login__btn__google'><div className='login__google' /> Продовжити з Gmail</button>
+                        <button onClick={HandleClick} className='login__btn__inst'><div className='login__inst' /> Продовжити з Instagram</button>
+                        <button onClick={HandleClick} className='login__btn__facebook'><div className='login__facebook' /> Продовжити з Facebook</button>
+                        <span>або</span><br />
+                        <div className='login__btn__wrap'>
+                            <button onClick={showRegistration}>Зареєструватися</button>
+                            <button onClick={showEnterence}>Увійти</button>
+                        </div>
+                        <Field
+                            type="email"
+                            id="email"
+                            name="email"
+                            onChange={handleChange}
+                            value={values.email}
+                            placeholder='Електронна пошта чи телефон'
+                            autoComplete="email"
+                        /><br />
+                        <Field
+                            type="password"
+                            id="password"
+                            name="password"
+                            onChange={handleChange}
+                            value={values.password}
+                            placeholder='Пароль*'
+                            autoComplete="password"
+                        /><br />
+                        {!registration ?
+                            <button
+                                className='login__forgot__pasword'
+                                onClick={HandleClick}>Забули пароль?
+                            </button> :
+                            <div className='login__checkbox'>
+                            <BootstrapForm.Group controlId="formCheckbox" className="d-flex align-items-center">
+                                <BootstrapForm.Check
+                                    type="checkbox"
+                                    name="checkbox"
+                                    checked={values.checkbox}
+                                    onChange={handleChange}
+                                />
+                                <BootstrapForm.Label className="ml-2">
+                                    Створюючи профіль на QuickQlick, ви погоджуєтеся з умовами використання
+                                </BootstrapForm.Label>
+                                <ErrorMessage name="checkbox" component="div" />
+                            </BootstrapForm.Group>
+                            </div>}
+                        <button
+                            type="submit"
+                            className='login__summit'>
+                            {registration ? 'Створити' : 'Увійти'}
+                        </button>
+                    </Form>
+                )}
+            </Formik>
+        </div>
     )
 }
 
