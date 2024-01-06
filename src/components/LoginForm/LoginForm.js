6b2c2416ca6abdf6126033a6c31fb6e511ce6fd5
Владимир <davidvovchik@gmail.com>
2024-01-06 13:23:00 +0200

    fix

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -7,6 +7,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 const LoginForm = () => {
     const [registration, setRegistration] = useState(false);
+    const [isEye, setIsEye] = useState(false);
+    const [emptyFields, setEmptyFields] = useState([]);
+
     const navigate = useNavigate();
 
     const handleNavigate = () => navigate("/");
@@ -25,6 +28,12 @@ const LoginForm = () => {
         setRegistration(false);
     }
 
+    const showPassword = () => {
+        setIsEye((prevState) => {
+            return !prevState;
+        });
+    };
+
     return (
         <div className='login__form__wrap'>
             <Formik
@@ -33,57 +42,105 @@ const LoginForm = () => {
                     password: '',
                     checkbox: false,
                 }}
-                onSubmit={values => {
-                    alert(JSON.stringify(values, null, 2));
+                onSubmit={(values, { resetForm }) => {
+                    const passwordRegex = /^[a-zA-Z!@#$%^&*()_+{}|:"<>?[\],.';~`]+$/;
+
+                    const emptyFieldsArray = Object.entries(values)
+                        .filter(([key, value]) => (key === 'email' ||
+                            (key === 'password' && !passwordRegex.test(value))) && !value)
+                        .map(([key]) => key);
+
+                    setEmptyFields(emptyFieldsArray);
+
+                    if (emptyFieldsArray.length === 0) {
+                        if (registration) {
+                            alert(JSON.stringify(values, null, 2));
+                        } else {
+                            const combinedValues = { email: values.email, password: values.password };
+                            alert(JSON.stringify(combinedValues, null, 2));
+                        }
+
+                        resetForm();
+                    }
                 }}
             >
                 {({ handleSubmit, handleChange, values }) => (
                     <Form onSubmit={handleSubmit} className='login__formik'>
-                        <button className='login__btn__back' onClick={handleNavigate}></button>
-                        <button onClick={HandleClick} className='login__btn__google'><div className='login__google' /> Продовжити з Gmail</button>
-                        <button onClick={HandleClick} className='login__btn__inst'><div className='login__inst' /> Продовжити з Instagram</button>
-                        <button onClick={HandleClick} className='login__btn__facebook'><div className='login__facebook' /> Продовжити з Facebook</button>
+                        <button
+                            className='login__btn__back'
+                            onClick={handleNavigate}>
+
+                        </button>
+                        <button
+                            onClick={HandleClick}
+                            className='login__btn__google'>
+                            <div className='login__google' />
+                            Продовжити з Gmail
+                        </button>
+                        <button
+                            onClick={HandleClick}
+                            className='login__btn__inst'>
+                            <div className='login__inst' />
+                            Продовжити з Instagram
+                        </button>
+                        <button
+                            onClick={HandleClick}
+                            className='login__btn__facebook'>
+                            <div className='login__facebook' />
+                            Продовжити з Facebook
+                        </button>
                         <span>або</span><br />
                         <div className='login__btn__wrap'>
                             <button onClick={showRegistration}>Зареєструватися</button>
                             <button onClick={showEnterence}>Увійти</button>
                         </div>
-                        <Field
-                            type="email"
-                            id="email"
-                            name="email"
-                            onChange={handleChange}
-                            value={values.email}
-                            placeholder='Електронна пошта чи телефон'
-                            autoComplete="email"
-                        /><br />
-                        <Field
-                            type="password"
-                            id="password"
-                            name="password"
-                            onChange={handleChange}
-                            value={values.password}
-                            placeholder='Пароль*'
-                            autoComplete="password"
-                        />
+                        <div className='login__wrap__email'>
+                            <Field
+                                type="email"
+                                id="email"
+                                name="email"
+                                onChange={handleChange}
+                                value={values.email}
+                                placeholder='Електронна пошта чи телефон'
+                                autoComplete="email"
+                                style={{ border: emptyFields.includes('email') ? '2px solid red' : '' }}
+                            />
+                        </div><br />
+                        <div className='login__wrap__password'>
+                            <Field
+                                type={!isEye ? "password" : "text"}
+                                id="password"
+                                name="password"
+                                onChange={handleChange}
+                                value={values.password}
+                                placeholder='Пароль*'
+                                autoComplete="password"
+                                style={{ border: emptyFields.includes('password') ? '2px solid red' : '' }}
+                            />
+                            <button
+                                type='button'
+                                onClick={showPassword}
+                                className={!isEye ? 'login__eye__close' : 'login__eye__open'}>
+                            </button>
+                        </div>
                         {!registration ?
                             <button
                                 className='login__forgot__pasword'
                                 onClick={HandleClick}>Забули пароль?
                             </button> :
                             <div className='login__checkbox'>
-                            <BootstrapForm.Group controlId="formCheckbox" className="d-flex align-items-center">
-                                <BootstrapForm.Check
-                                    type="checkbox"
-                                    name="checkbox"
-                                    checked={values.checkbox}
-                                    onChange={handleChange}
-                                />
-                                <BootstrapForm.Label className="ml-2">
-                                    Створюючи профіль на QuickQlick, ви погоджуєтеся з умовами використання
-                                </BootstrapForm.Label>
-                                <ErrorMessage name="checkbox" component="div" />
-                            </BootstrapForm.Group>
+                                <BootstrapForm.Group controlId="formCheckbox" className="d-flex align-items-center">
+                                    <BootstrapForm.Check
+                                        type="checkbox"
+                                        name="checkbox"
+                                        checked={values.checkbox}
+                                        onChange={handleChange}
+                                    />
+                                    <BootstrapForm.Label className="ml-2">
+                                        Створюючи профіль на QuickQlick, ви погоджуєтеся з умовами використання
+                                    </BootstrapForm.Label>
+                                    <ErrorMessage name="checkbox" component="div" />
+                                </BootstrapForm.Group>
                             </div>}
                         <button
                             type="submit"
